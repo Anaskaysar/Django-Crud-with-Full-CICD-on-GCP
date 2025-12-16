9ed121b4f0e98cc800f4e2f6a31c98143878ddd6
anaskaysar <kaysarul.apurba@northsouth.edu>
2025-12-16 18:53:15 -0500

    Add full home page with create and list items

--- crudproject/items/views.py
+++ crudproject/items/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Item
 
 def home(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "").strip()
+        description = request.POST.get("description", "").strip()
+
+        if name:
+            Item.objects.create(name=name, description=description)
+            return redirect("items-home")  # redirect after POST
+
     items = Item.objects.all().order_by("-created_at")
     return render(request, "items/home.html", {"items": items})
