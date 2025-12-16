6f65b5ca72399dfc7c761fb4e9917ed295e5ac19
anaskaysar <kaysarul.apurba@northsouth.edu>
2025-12-16 18:46:48 -0500

    Admin Dash and Data Input

--- crudproject/items/views.py
+++ crudproject/items/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from .models import Item
 
 def home(request):
-    return render(request, "items/home.html")
-
+    items = Item.objects.all().order_by("-created_at")
+    return render(request, "items/home.html", {"items": items})
