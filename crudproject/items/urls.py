e1bd0989b98b7921082766ee8a27a1bbb0b31346
anaskaysar <kaysarul.apurba@northsouth.edu>
2025-12-16 19:22:37 -0500

    Add styled d=item dashboard with create and delete

--- crudproject/items/urls.py
+++ crudproject/items/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, delete_item
 
 urlpatterns = [
     path("", home, name="items-home"),
+    path("delete/<int:item_id>/", delete_item, name="items-delete"),
 ]
